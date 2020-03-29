e8732a0e47eb7af23e4129dcf570397a9e2936e9
trongdatdinh1 <trongdatdinh1@gmail.com>
2020-03-29 13:07:16 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a535543f875ae31dac9507154c1d878b.js"
+  "/vn-tch1608-team5-etutor-web-service-frontend/precache-manifest.b5685462d6ecda9bd7b12a385ff6a062.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vn-tch1608-team5-etutor-web-service-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
